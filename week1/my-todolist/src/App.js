3956f4d09925bbeb79d1c83c2537c0a6ab7747ab
stephanieanahi <stephanie.guzman@bryanuniversity.edu>
2022-06-03 03:16:02 -0500

    modified files

--- week1/my-todolist/src/App.js
+++ week1/my-todolist/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
+import TodoList from './TodoList.js';
+ export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
         <h1>TO DO LIST</h1>
+    <TodoList />
+      
     
-        <ul className="myList">
-         <li input type="checkbox" id="myList" name="topping" value="done">feed dogs</li>
-         <li>take kids to school</li>
-         <li>go grocery shopping</li>
-         <li>clean house</li>
-         <li>pick up kids from school</li>
-         <li>take kids to practice</li>
-        </ul>
-    </header>
     </div>
   );
 }
 
-export default App;
+
---
+++ week1/my-todolist/src/TodoList.js
@@ -0,0 +1,22 @@
+
+function App(TodoList) {
+
+return (
+    <ul >
+      <li>
+        <input type="checkbox" /> go to gym
+      </li>
+      <li>
+        <input type="checkbox" /> feed dogs
+      </li>
+      <li>
+        <input type="checkbox" /> take kids to school
+      </li>
+      <li>
+        <input type="checkbox" /> go to work
+      </li>
+    </ul> 
+)
+
+};
+export default App;
